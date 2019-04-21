f063392b8d5a50caf5f16246a10a3af072e9b9a7
Tejas Cousik <tcousik0@gmail.com>
2019-04-21 18:48:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   words = string.split(" ")
   new_array = []
   words.each do |stuff|
-    new_array << stuff.reverse!
+    new_array << stuff.reverse
   end
   return new_array
 end
